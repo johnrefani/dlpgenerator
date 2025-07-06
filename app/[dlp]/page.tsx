ec69a7a4e5bbc081f2da6a97de8b32dde3002a32
John Rey Luntayao <johnrey.luntayao23@gmail.com>
2025-07-06 11:28:01 +0800

    set up object data and result page

---
+++ app/[dlp]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const getLessonPlan = () => {
+  return (
+    <section>
+        mamamo
+    </section>
+  )
+}
+
+export default getLessonPlan
---
+++ data/index.ts
@@ -0,0 +1,105 @@
+export const dlpData = [
+    {
+        id: 1,
+        paaralan: "",
+        pangalanNgGuro: "",
+        petsaAtOras: "",
+        lessonPlan: [
+            {
+                id: 1,
+                baitang: "",
+                asignatura: "",
+                markahan: "",
+                getLessonPlan: [
+                    {
+                        id: 1,
+                        title: "I. NILALAMAN NG KURIKULUM, PAMANTAYAN, AT MGA KASANAYAN SA ARALIN",
+                        content: [
+                            {
+                                id: 1,
+                                title: "A. Mga Pamantayang Pangnilalaman",
+                                impormasyon: ""
+                            },{
+                                id: 2,
+                                title: "B. Mga Pamantayan sa Pagganap",
+                                impormasyon: ""
+                            },{
+                                id: 3,
+                                title: "C. Mga Kasanayan at Layuning Pampagkatuto",
+                                impormasyon: ""
+                            },{
+                                id: 4,
+                                title: "D. Nilalaman",
+                                impormasyon: ""
+                            },{
+                                id: 5,
+                                title: "E. Pagpapahalagang Lilinangin",
+                                impormasyon: ""
+                            },{
+                                id: 6,
+                                title: "F. Integrasyon",
+                                impormasyon: ""
+                            },
+                        ],
+                    },{
+                        id: 2,
+                        title: "II. BATAYANG SANGGUNIAN SA PAGKATUTO",
+                        content: [
+                            {
+                                id: 1,
+                                impormasyon: "",
+                            },
+                        ],
+                    },{
+                        id: 3, 
+                        title: "III. MGA HAKBANG SA PAGTUTURO AT PAGKATUTO",
+                        content: [
+                            {
+                                id: 1,
+                                title: "A. Pagkuha ng Dating Kaalaman",
+                                impormasyon: "",
+                                mgaTala: "",
+                            },{
+                                id: 2,
+                                title: "B. Paglalahad ng Layunin",
+                                impormasyon: "",
+                                mgaTala: "",
+                            },{
+                                id: 3,
+                                title: "C. Paglinang at Pagpapalalim",
+                                impormasyon: "",
+                                mgaTala: "",
+                            },{
+                                id: 4,
+                                title: "D. Paglalahat",
+                                impormasyon: "",
+                                mgaTala: "",
+                            },
+                        ]
+                    },{
+                        id: 4,
+                        title: "IV. EBALWAYSON NG PAGKATUTO: PAGTATAYA AT PAGNINILAY",
+                        content: [
+                           {
+                                id: 1,
+                                title: "A. Pagtataya",
+                                impormasyon: "",
+                                mgaTala: "",
+                            },{
+                                id: 2,
+                                title: "B. Pagbuo ng Anotasyon",
+                                impormasyon: "",
+                                mgaTala: "",
+                            },{
+                                id: 3,
+                                title: "C. Pagninilay",
+                                impormasyon: "",
+                                mgaTala: "",
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+];
