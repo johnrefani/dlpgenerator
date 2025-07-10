cf490a587fb45622b7f2822ffa87298150e2bcca
John Rey Luntayao <johnrey.luntayao23@gmail.com>
2025-07-10 15:11:45 +0800

    add responsiveness to main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { InfoForm } from "@/lib/imports";
 
 export default function Home() {
   return (
-   <main className="flex-center flex-col my-8 space-y-6 md:space-y-7 lg:space-y-8">
+   <main className="flex-1 my-8 space-y-6 mx-[16px] md:mx-[64px] lg:mx-[120px] place-items-center">
       <h1 className="text-3xl text-white font-bold text-center">DAILY LESSON LOG</h1>
       <InfoForm />
    </main>
